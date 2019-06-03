56c070f57bca3eb74ff256316b6f032fc163c73e
pawlooss1 <pawlooss13@gmail.com>
2019-06-03 19:57:45 +0200

    Time conflict finished, main.py added

---
+++ main.py
@@ -0,0 +1,6 @@
+from time_conflict import find_time_conflicts
+
+
+find_time_conflicts('2019-20203.xlsx', 'zima-s')
+find_time_conflicts('2019-20203.xlsx', 'lato-s')
+find_time_conflicts('2019-20203.xlsx', 'zima-n', stationary=False)
--- tools.py
+++ tools.py
@@ -6,10 +6,31 @@ def load_sheet(file_name, sheet_name):
     return pd.read_excel(file_name, sheet_name)
 
 
-def shrink_df(df, columns_to_leave):
-    # wyrzuca puste wiersze i zostawia tylko kolumny z listy columns_to_leave
+def clean_df(df):
+    # wyrzuca puste wiersze, a kolumny dzien i tyg
+    # uzupelnia wartosciami domyslnymi (zeby nie bylo NaN i z nich skorzystac)
     df = df.dropna(thresh=2)
-    return df[columns_to_leave]
+    return df.fillna(value={'dzien': 'noday', 'tyg': 'AB'})
+
+
+def find_non_stationary_days_index(column_names):
+    for i in range(len(column_names)):
+        if column_names[i] == 'uwagi':
+            return i + 1
+
+
+def check_week(lecture, classes):
+    values = []
+    for i, c in classes.iterrows():
+        values.append(c['tyg'] in lecture['tyg'] or lecture['tyg'] in c['tyg'])
+    return pd.Series(data=values, index=classes.index)
+
+
+def fold_true(row):
+    acc = False
+    for name in row.index:
+        acc = acc or row[name]
+        return acc
 
 
 def add_90_minutes(start_time):
@@ -20,66 +41,9 @@ def add_90_minutes(start_time):
     return full_end.time()  # zwroc sam czas
 
 
-def check_pair_conflict(lecture_start, lecture_end, class_to_check):
-    # sprawdz pare ktora jest w tym samym dniu (+ ew. w odpowiednim tygodniu)
-    class_start = class_to_check['godz']
-    if not isinstance(class_start, datetime.time):
-        return False
-    if not isinstance(class_to_check['koniec'], datetime.time):
-        class_end = add_90_minutes(class_start)
-    else:
-        class_end = class_to_check['koniec']
-    if lecture_start <= class_end <= lecture_end or lecture_start <= class_start <= lecture_end:
-        return True
-    else:
-        return False
-
-
 def print_conflict(lecture_index, lecture, class_index, class_conf):
     statement = 'Wiersz {} ({}, {} {}) koliduje z\n' \
                 'wierszem {} ({}, {} {})\n'.format(lecture_index + 2, lecture['przedmiot'], lecture['sala'],
                                                    lecture['godz'], class_index + 2, class_conf['przedmiot'],
                                                    class_conf['sala'], class_conf['godz'])
     print(statement)
-
-
-def find_conflict_with_lecture(lecture_index, lecture, classes):
-    day = lecture['dzien']
-    lecture_start = lecture['godz']
-    if not isinstance(day, str) or not isinstance(lecture_start, datetime.time):
-        return
-    if not isinstance(lecture['koniec'], datetime.time):
-        lecture_end = add_90_minutes(lecture_start)
-    else:
-        lecture_end = lecture['koniec']
-    if not isinstance(lecture['tyg'], str):  # bez podzialu a/b
-        classes_to_check = classes[classes['dzien'] == day]
-    else:  # z podzialem a/b
-        classes_to_check = classes[(classes['dzien'] == day) & (classes['tyg'] == lecture['tyg'])]
-    for class_index, row in classes_to_check.iterrows():
-        if check_pair_conflict(lecture_start, lecture_end, row):
-            print_conflict(lecture_index, lecture, class_index, row)
-
-
-def find_time_conflicts_semesters(df):
-    lectures = df[df['typ'] == 'W']
-    classes = df[(df['typ'] == 'L') | (df['typ'] == 'C') | (df['typ'] == 'P')]
-    for index, row in lectures.iterrows():
-        find_conflict_with_lecture(index, row, classes)
-
-
-def find_time_conflicts_class_types(df):
-    semesters = df['sem'].unique()
-    for semester in semesters:
-        tmp_df = df[df['sem'] == semester]
-        find_time_conflicts_semesters(tmp_df)
-
-
-def find_time_conflicts(file_name, sheet_name):
-    sheet = load_sheet(file_name, sheet_name)
-    df = shrink_df(sheet, ['studia', 'sem', 'przedmiot', 'typ', 'sala', 'tyg', 'dzien', 'godz', 'koniec'])
-    class_types = df['studia'].unique()
-    for class_type in class_types:
-        tmp_df = df[df['studia'] == class_type]
-        find_time_conflicts_class_types(tmp_df)
-    # return df
