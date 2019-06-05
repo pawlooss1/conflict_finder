6d098cdac9efe0be3deb1e8a8715c7c934ba64a4
pawlooss1 <pawlooss13@gmail.com>
2019-06-05 22:24:01 +0200

    Little changes in main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from time_conflict import find_time_conflicts
+from room_conflict import finder
 
-
-find_time_conflicts('2019-20203.xlsx', 'zima-s')
-find_time_conflicts('2019-20203.xlsx', 'lato-s')
-find_time_conflicts('2019-20203.xlsx', 'zima-n', stationary=False)
+# find_time_conflicts('2019-20203.xlsx', 'zima-s')
+# find_time_conflicts('2019-20203.xlsx', 'lato-s')
+# find_time_conflicts('2019-20203.xlsx', 'zima-n', stationary=False)
+finder('2019-20203.xlsx', 'zima-s', 'zima-inne')
